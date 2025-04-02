50f05c06faa6e44ae2014c3ddda2000aa77dcd02
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-02 17:15:11 +0100

    chore: include favicon :fire:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Open_Sans, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
@@ -19,6 +19,17 @@ export const metadata: Metadata = {
     template: "%s | Nnamdi Azubuike",
   },
   description: "Nnamdi Azubuike's Portfolio, a software engineer focused on building scalable and efficient applications",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/svg",
+      url: "./favicon.svg",
+    },
+    {
+      rel: "apple",
+      url: "./favicon.svg",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -29,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased,`}
+        className={`${robotoMono.variable} ${openSans.variable} font-sans antialiased`}
       >
         <ThemeProvider
           attribute="class"
