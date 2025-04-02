0f7ecc10c5a1033e3645df626e252f96f16d7068
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-04-02 18:40:46 +0100

    chore: include image url and pwa :fire:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
-
+import withPWA from 'next-pwa';
 const nextConfig: NextConfig = {
-  /* config options here */
+  ...withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }),
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.hashnode.com',
+        port: '',
+        pathname: '/res/hashnode/image/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
