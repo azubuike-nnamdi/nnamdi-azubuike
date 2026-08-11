61b118ce2b32b23a08595afadc0fcb10cb2a73f6
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-08-11 08:11:47 +0100

    refactor(app): move projects page into frontend group

--- app/(frontend)/(root)/projects/page.tsx
+++ app/(frontend)/(root)/projects/page.tsx
@@ -1,12 +1,13 @@
 import ProjectLanding from '@/components/projects/project-landing'
+import { getProjects } from '@/lib/cms'
 
 export const metadata = {
   title: 'Projects - Nnamdi Azubuike',
   description: 'Projects - Nnamdi Azubuike',
 }
 
-export default function Page() {
-  return (
-    <ProjectLanding />
-  )
+export default async function Page() {
+  const projects = await getProjects()
+
+  return <ProjectLanding projects={projects} />
 }
